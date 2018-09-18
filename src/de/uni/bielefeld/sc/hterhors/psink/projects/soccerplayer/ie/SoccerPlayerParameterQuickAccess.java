d493ad921db70371b75b271fd6616f2e0a945816
Hendrik <hterhors@techfak.uni-bielefeld.de>
2018-09-18 13:14:41 +0200

    Add comments and java doc

--- src/de/uni/bielefeld/sc/hterhors/psink/projects/soccerplayer/ie/SoccerPlayerParameterQuickAccess.java
+++ src/de/uni/bielefeld/sc/hterhors/psink/projects/soccerplayer/ie/SoccerPlayerParameterQuickAccess.java
@@ -5,7 +5,6 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-import de.uni.bielefeld.sc.hterhors.psink.obie.core.projects.AbstractOBIEProjectEnvironment;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.corpus.distributor.AbstractCorpusDistributor;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.corpus.distributor.ActiveLearningDistributor;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.corpus.distributor.FoldCrossCorpusDistributor;
@@ -19,7 +18,7 @@ import de.uni.bielefeld.sc.hterhors.psink.obie.ie.explorer.IExplorationCondition
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.explorer.SlotCardinalityExplorer;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.explorer.TemplateExplorer;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.InvestigationRestriction;
-import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.param.EInitializer;
+import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.param.EInstantiationType;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.param.IInitializeNumberOfObjects;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.param.OBIERunParameter.OBIEParameterBuilder;
 import de.uni.bielefeld.sc.hterhors.psink.obie.projects.soccerplayer.ontology.interfaces.ISoccerPlayer;
@@ -51,82 +50,169 @@ public class SoccerPlayerParameterQuickAccess {
 
 	public static OBIEParameterBuilder getREParameter() {
 
-		final AbstractOBIEProjectEnvironment environment = SoccerPlayerProjectEnvironment.getInstance();
-
-		final AbstractCorpusDistributor corpusConfiguration = originDist();
-
+		/**
+		 * Some personal notes to this run.
+		 */
 		final String personalNote = "Reasoning Web Summer School 2018 Lecture";
 
+		/**
+		 * the rott directory to where the model should be saved.
+		 */
 		final File rootDirectory = new File("./");
 
-		final int epochs = 100;
-
+		/**
+		 * The prefix name of the corpus. TODO: handle different.
+		 */
 		final String corpusNamePrefix = "small";
 
-		IExplorationCondition explorationCondition = (a, b, c) -> true;
+		/**
+		 * The type of the instantiated main templates. Use empty if the templates
+		 * should be created empty in new states. For more types see EInstantiationType.
+		 */
+		final EInstantiationType initializer = EInstantiationType.EMPTY;
 
-		final EInitializer initializer = EInitializer.EMPTY;
+		/**
+		 * The number of main templates that should be instantiated initially for empty
+		 * states.
+		 */
+		final IInitializeNumberOfObjects numberOfInitializedObjects = instance -> 1;
 
-		IInitializeNumberOfObjects numberOfInitializedObjects = instance -> 1;
-
-		int numberOfMaxSamplingSteps = 100;
-
+		/**
+		 * A template may have a slot that filler has a slot that iller has a slot and
+		 * so on. This restricts the maximum evaluation depth. The depth is equal to the
+		 * number of sub-slots. Set this to 1 if you want e.g. just the main slots of
+		 * the template to be evaluated. For specific evaluation restrictions specify
+		 * restrictions in the InvestigationRestiction.
+		 */
 		final int maxEvaluationDepth = Integer.MAX_VALUE;
+
+		/**
+		 * Whether the number of instances in collections-typed slots should be part of
+		 * the evaluation or not. In realcase Scenario this needs to be set to true.
+		 * 
+		 * If set to false the actual number of elements in a list or number of
+		 * instantiated templates is not evaluated.
+		 */
 		final boolean penalizeCardinality = true;
-		final boolean enableCaching = true;
 
-		int maxNumberOfEntityElements = 3;
-		int maxNumberOfDataTypeElements = 3;
+		/**
+		 * Whether caching should be activated during evaluation or not. If your system
+		 * provides much RAm you might consider setting this to true to speed up the
+		 * evaluation process. This makes especially sense when using the
+		 * CartesianSearchEvaluator.
+		 */
+		final boolean enableEvaluationCaching = true;
 
-		Random rndForSampling = new Random(100L);
+		/**
+		 * The maximum number of entity elements per list or maximum number of
+		 * instances. This variable restricts the sampling procedure and needs to be
+		 * adjust to the dataset.
+		 */
+		final int maxNumberOfEntityElements = 3;
 
-		boolean ignoreEmptyInstancesOnEvaluation = false;
+		/**
+		 * Maximum number of datatype entities in a collection slot that takes datatype
+		 * entities as filler. This restricts the sampling procedure and needs to be
+		 * adjust to the dataset.
+		 */
+		final int maxNumberOfDataTypeElements = 3;
 
-		// IEvaluator evaluator = new BeamSearchPRF1(enableCaching,
-		// maxEvaluationDepth, penalizeCardinality,
-		// sampleRestrictions, new SCIOOrListConditon(),
-		// maxNumberOfEntityElements,
-		// ignoreEmptyInstancesOnEvaluation);
+		/**
+		 * Use this class to specify a fine grained investigation restriction that
+		 * affects not only the evaluation but also the sampling procedure.
+		 * 
+		 * You can add a concrete specification of what slots should be investigated or
+		 * not.
+		 * 
+		 * USE WITH CAUTION! See documentation of InvestigationRestiction for remaining
+		 * todos.
+		 * 
+		 */
+		final InvestigationRestriction investigationRestiction = InvestigationRestriction.noRestrictionInstance;
 
-		IEvaluator evaluator = new CartesianSearchEvaluator(enableCaching, maxEvaluationDepth, penalizeCardinality,
-				InvestigationRestriction.noRestrictionInstance, new DatatypeOrListConditon(), maxNumberOfEntityElements,
+		/**
+		 * Fix the randomization of the sampling procedure.
+		 */
+		final Random rndForSampling = new Random(100L);
+
+		/**
+		 * Whether empty created instances should be removed from the evaluation
+		 * beforehand. If this is set to true, a state with hundreds of empty instances
+		 * and one filled is as good as a state that have only the filled instance.
+		 *
+		 * During training and prediction this should be set to false. You might
+		 * consider setting this variable to true in the final evaluation.
+		 */
+		final boolean ignoreEmptyInstancesOnEvaluation = false;
+
+		/**
+		 * The evaluation type. CartesianSearch Evaluator is the most precise evaluation
+		 * as it computes the exact score. In some cases, if the data set is to large or
+		 * the number of elements in lists are to high, this evaluation may be to slow
+		 * as it needs to compute the factorial number of comparisons.
+		 *
+		 * Try instead BeamSearchEvaluator or PurityEvaluator.
+		 *
+		 */
+		final IEvaluator evaluator = new CartesianSearchEvaluator(enableEvaluationCaching, maxEvaluationDepth,
+				penalizeCardinality, investigationRestiction, new DatatypeOrListConditon(), maxNumberOfEntityElements,
 				ignoreEmptyInstancesOnEvaluation);
 
-		Set<Class<? extends AbstractOBIEExplorer>> explorerTypes = new HashSet<>();
+		/**
+		 * The exploration strategies.
+		 */
+		final Set<Class<? extends AbstractOBIEExplorer>> explorerTypes = new HashSet<>();
 
-		// explorerTypes.add(NoChangeExplorer.class);
+		/**
+		 * Uses the ontology for creating proposal states. Template slots are filled by
+		 * possible slot filler that are defined in the ontology. Per default
+		 * ontological classes that were not found in the document (via named entity
+		 * recognition and linking) are removed from the candidate set.
+		 *
+		 * Datatype slots (marked with the @DatatypeProperty.java) annotation can only
+		 * be filled with previously found datatype entities!
+		 *
+		 * The TemplateExplorer can be parameterized via the OBIEParamterBuilder!
+		 *
+		 */
 		explorerTypes.add(TemplateExplorer.class);
-//		explorerTypes.add(DependentCardinalityExplorer.class);
 
+		/**
+		 * This explorer explores the cardinality of slots that can have more than one
+		 * entry. Such slots (java class fields) are makred with the
+		 * annotation @RelationTypeCollection.java
+		 */
 		explorerTypes.add(SlotCardinalityExplorer.class);
-		// explorerTypes.add(TemplateCardinalityExplorer.class);
 
-		return new OBIEParameterBuilder().setCorpusDistributor(corpusConfiguration)
-				.setCorpusNamePrefix(corpusNamePrefix).setEpochs(epochs).setExplorationCondition(explorationCondition)
-				.setExplorers(explorerTypes).setInitializer(initializer)
-				.setNumberOfInitializedObjects(numberOfInitializedObjects)
-				.setNumberOfMaxSamplingSteps(numberOfMaxSamplingSteps).setPersonalNotes(personalNote)
-				.setRootDirectory(rootDirectory).addRootSearchType(searchType).setEnvironment(environment)
+		/**
+		 * There are many more parameter to check out:
+		 */
+		return new OBIEParameterBuilder().setCorpusNamePrefix(corpusNamePrefix).setExplorers(explorerTypes)
+				.setInitializer(initializer).setNumberOfInitializedObjects(numberOfInitializedObjects)
+				.setPersonalNotes(personalNote).setRootDirectory(rootDirectory).addRootSearchType(searchType)
 				.setEvaluator(evaluator).setMaxNumberOfEntityElements(maxNumberOfEntityElements)
 				.setMaxNumberOfDataTypeElements(maxNumberOfDataTypeElements).setRandomForSampling(rndForSampling);
 
 	}
 
-	public static AbstractCorpusDistributor shuffleDist() {
-		return new ShuffleCorpusDistributor.Builder().setTrainingProportion(80).setDevelopmentProportion(0)
-				.setTestProportion(20).build();
-	}
+	public static class preDefinedCorpusDistributor {
 
-	public static AbstractCorpusDistributor originDist() {
-		return new OriginalCorpusDistributor.Builder().build();
-	}
+		public static AbstractCorpusDistributor shuffleDist() {
+			return new ShuffleCorpusDistributor.Builder().setTrainingProportion(80).setDevelopmentProportion(0)
+					.setTestProportion(20).build();
+		}
 
-	public static AbstractCorpusDistributor foldCrossDist() {
-		return new FoldCrossCorpusDistributor.Builder().setN(10).setSeed(12345L).build();
-	}
+		public static AbstractCorpusDistributor originDist() {
+			return new OriginalCorpusDistributor.Builder().build();
+		}
 
-	public static AbstractCorpusDistributor activeLearningDist() {
-		return new ActiveLearningDistributor.Builder().setB(1).setSeed(100L).setInitialTrainingSelectionFraction(0.01)
-				.setTrainingProportion(80).setTestProportion(20).build();
+		public static AbstractCorpusDistributor foldCrossDist() {
+			return new FoldCrossCorpusDistributor.Builder().setN(10).setSeed(12345L).build();
+		}
+
+		public static AbstractCorpusDistributor activeLearningDist() {
+			return new ActiveLearningDistributor.Builder().setB(1).setSeed(100L)
+					.setInitialTrainingSelectionFraction(0.01).setTrainingProportion(80).setTestProportion(20).build();
+		}
 	}
 }
--- src/de/uni/bielefeld/sc/hterhors/psink/projects/soccerplayer/ie/StartExtraction.java
+++ src/de/uni/bielefeld/sc/hterhors/psink/projects/soccerplayer/ie/StartExtraction.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import de.uni.bielefeld.sc.hterhors.psink.obie.core.evaluation.PRF1Container;
+import de.uni.bielefeld.sc.hterhors.psink.obie.core.projects.AbstractOBIEProjectEnvironment;
+import de.uni.bielefeld.sc.hterhors.psink.obie.ie.corpus.distributor.AbstractCorpusDistributor;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.AbstractOBIERunner;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.StandardRERunner;
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.run.param.OBIERunParameter;
@@ -16,8 +18,14 @@ import de.uni.bielefeld.sc.hterhors.psink.obie.ie.templates.SlotIsFilledTemplate
 import de.uni.bielefeld.sc.hterhors.psink.obie.ie.templates.TokenContextTemplate;
 
 /**
+ * 
+ * Read README.md for more and detailed information.
+ * 
  *
- * Preferred VM parameter: -Xmx12g -XX:+UseG1GC -XX:+UseStringDeduplication
+ * Preferred VM run-parameter: -Xmx12g -XX:+UseG1GC -XX:+UseStringDeduplication
+ * 
+ * Main starting class for the information extraction task using the
+ * SoccerPlayerOntology and SoccerPlayer-Wikipedia data set.
  * 
  * @author hterhors
  *
@@ -34,32 +42,81 @@ public class StartExtraction {
 	 * The runID. This serves as an identifier for locating and saving the model. If
 	 * anything was changed during the development the runID should be reset.
 	 */
-	public final static String runID = "-birthYearTemplate";
+	private final static String runID = "-birthYearTemplate2";
+	/**
+	 * The systems environment.
+	 */
+	private final AbstractOBIEProjectEnvironment environment = SoccerPlayerProjectEnvironment.getInstance();
 
 	public StartExtraction() throws Exception {
 
-		OBIEParameterBuilder paramBuilder = SoccerPlayerParameterQuickAccess.getREParameter();
+		/**
+		 * This parameterBuilder contains standard configurations of the system that are
+		 * used for Relation Extraction tasks. You can but may not change the parameter
+		 * predefined in here unless you know what you are doing!
+		 */
+		final OBIEParameterBuilder paramBuilder = SoccerPlayerParameterQuickAccess.getREParameter();
 
-		paramBuilder.setCorpusDistributor(SoccerPlayerParameterQuickAccess.originDist());
+		/*
+		 * Add required missing parameter...
+		 */
+
+		/**
+		 * The number of epochs that the system should be trained.
+		 */
+		final int epochs = 10;
+
+		/**
+		 * The distribution of the documents in the corpus. Origin takes training ,
+		 * development and test data as they are. Use shufflDist() for shuffle the
+		 * documents before and redistribute to train (80%), dev(0%) and test(20%). (You
+		 * may change that distribution by building your own distributor...
+		 */
+		final AbstractCorpusDistributor corpusDistributor = SoccerPlayerParameterQuickAccess.preDefinedCorpusDistributor
+				.originDist();
+
+		paramBuilder.setCorpusDistributor(corpusDistributor);
 		paramBuilder.setRunID(runID);
+		paramBuilder.setEnvironment(environment);
+		paramBuilder.setEpochs(epochs);
 
+		/*
+		 * Add factor-graph-templates.
+		 */
 		addTemplates(paramBuilder);
 
+		/*
+		 * Build parameter.
+		 */
 		OBIERunParameter parameter = paramBuilder.build();
 
+		/*
+		 * Created new standard Relation Extraction runner.
+		 */
 		AbstractOBIERunner runner = new StandardRERunner(parameter);
 
 		run(runner);
 	}
 
+	/**
+	 * Add templates to the parameter builder.
+	 * 
+	 * @param paramBuilder
+	 */
 	private void addTemplates(OBIEParameterBuilder paramBuilder) {
 
-		Set<Class<? extends AbstractOBIETemplate<?>>> templates = new HashSet<>();
+		final Set<Class<? extends AbstractOBIETemplate<?>>> templates = new HashSet<>();
 
-//		templates.add(FrequencyTemplate.class);
-
+		/**
+		 * Add your own templates:
+		 */
 //		templates.add(BirthYearTemplate.class);
 //		templates.add(PriorTemplate.class);
+
+		/**
+		 * Predefined and generic template:
+		 */
+//		templates.add(FrequencyTemplate.class);
 
 		templates.add(TokenContextTemplate.class);
 		templates.add(InterTokenTemplate.class);
@@ -67,32 +124,50 @@ public class StartExtraction {
 		templates.add(InBetweenContextTemplate.class);
 		templates.add(LocalTemplate.class);
 
-		/*
-		 * Cardinality
+		/**
+		 * Templates that capture the cardinality of slots
 		 */
 
 //		templates.add(MainSlotVarietyTemplate.class);
+
 		paramBuilder.setTemplates(templates);
 	}
-	// 100 Epochs
-//	Time needed: 10672169
-//	p: 0.8522702104097447	r: 0.7169988925802879	f1: 0.7788043438548948
 
+	/**
+	 * Run the system with the specifications and configurations.
+	 * 
+	 * @param runner
+	 * @throws Exception
+	 */
 	private static void run(AbstractOBIERunner runner) throws Exception {
 
-		long time = System.currentTimeMillis();
+		final long time = System.currentTimeMillis();
 
 		if (runner.modelExists()) {
+			/*
+			 * If the model exists, load the model from the file system. The model location
+			 * is specified in the parameter and the environment.
+			 * 
+			 * TODO: unify!
+			 */
 			runner.loadModel();
 		} else {
+			/*
+			 * If the model does not exists train. The model is automatically stored to the
+			 * file system to the given model location!
+			 */
 			runner.train();
 		}
 
-		PRF1Container pfr1 = runner.evaluateOnTest();
+		System.out.println("Time needed to train the model: " + (System.currentTimeMillis() - time) + " ms.");
 
-		System.out.println("Time needed: " + (System.currentTimeMillis() - time));
+		/**
+		 * Evaluate the trained model on the test data. This is equal to predictOnTest
+		 * and apply the results to an evaluator.
+		 */
+		final PRF1Container pfr1 = runner.evaluateOnTest();
 
-		System.out.println(pfr1);
+		System.out.println("Evaluation results on test data:\n" + pfr1);
 
 	}
 
