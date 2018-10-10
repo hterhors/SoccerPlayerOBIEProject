68c3420e187189cc806e3488aa93537e3cc15d23
Hendrik <hterhors@techfak.uni-bielefeld.de>
2018-10-10 15:19:16 +0200

    Add Random Baseline Example

---
+++ src/main/java/de/hterhors/obie/projects/soccerplayer/ie/baseline/ComputeRandomBaseline.java
@@ -0,0 +1,66 @@
+package de.hterhors.obie.projects.soccerplayer.ie.baseline;
+
+import java.util.Random;
+
+import de.hterhors.obie.core.evaluation.PRF1Container;
+import de.hterhors.obie.ml.corpus.distributor.FoldCrossCorpusDistributor;
+import de.hterhors.obie.ml.run.AbstractOBIERunner;
+import de.hterhors.obie.ml.run.StandardRERunner;
+import de.hterhors.obie.ml.run.param.OBIERunParameter;
+import de.hterhors.obie.ml.run.param.OBIERunParameter.Builder;
+import de.hterhors.obie.ml.tools.baseline.RandomBaseline;
+import de.hterhors.obie.projects.soccerplayer.ie.SoccerPlayerOntologyEnvironment;
+import de.hterhors.obie.projects.soccerplayer.ie.SoccerPlayerParameterQuickAccess;
+import de.hterhors.obie.projects.soccerplayer.ie.SoccerPlayerProjectEnvironment;
+
+public class ComputeRandomBaseline {
+
+	public static void main(String[] args) throws Exception {
+
+		nFoldCrossValidation();
+	}
+
+	private static void nFoldCrossValidation() throws Exception {
+		PRF1Container mean = new PRF1Container(0, 0, 0);
+
+		long allTime = System.currentTimeMillis();
+
+		Builder paramBuilder = SoccerPlayerParameterQuickAccess.getREParameter();
+
+		paramBuilder
+				.setCorpusDistributor(SoccerPlayerParameterQuickAccess.preDefinedCorpusDistributor.foldCrossDist(1F));
+
+		paramBuilder.setOntologyEnvironment(SoccerPlayerOntologyEnvironment.getInstance());
+		paramBuilder.setProjectEnvironment(SoccerPlayerProjectEnvironment.getInstance());
+
+		OBIERunParameter param = paramBuilder.build();
+		AbstractOBIERunner runner = new StandardRERunner(param);
+
+		final long initSeed = 100L;
+
+		while (runner.corpusProvider.nextFold()) {
+			
+			long seed = new Random().nextLong();
+
+			System.out.println("#############################");
+			System.out.println("New " + ((FoldCrossCorpusDistributor) runner.parameter.corpusDistributor).n
+					+ "-fold cross validation iteration: "
+					+ String.valueOf(runner.corpusProvider.getCurrentFoldIndex() + 1));
+			long time = System.currentTimeMillis();
+
+//			System.out.println("Set test instances to:");
+//			runner.corpusProvider.getTestCorpus().getInternalInstances().forEach(System.out::println);
+			System.out.println("#############################");
+			PRF1Container pfr1 = new RandomBaseline(param, seed).run(runner.corpusProvider.getTestCorpus());
+
+			mean = new PRF1Container((mean.p + pfr1.p) / 2, (mean.r + pfr1.r) / 2, (mean.f1 + pfr1.f1) / 2);
+			System.out.println("Time needed: " + (System.currentTimeMillis() - time));
+
+		}
+		System.out.println(
+				((FoldCrossCorpusDistributor) runner.parameter.corpusDistributor).n + " fold cross validation mean: ");
+		System.out.println(mean);
+		System.out.println("Time needed: " + (System.currentTimeMillis() - allTime));
+
+	}
+}
